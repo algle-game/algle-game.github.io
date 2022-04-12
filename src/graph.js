f02c9b7b3a057e1db5e431296136590579f8bf20
Arne (Home) <aschmidt@ibr.cs.tu-bs.de>
2022-04-12 08:35:54 +0200

    init game

---
+++ src/graph.js
@@ -0,0 +1,229 @@
+var version = 0.2;
+var num_tries = 0;
+var N = 0
+var svg = document.getElementById('area');
+var svgNS = svg.namespaceURI;
+var node = null;
+var incMatrix = null;
+var knownEdges = null;
+const date = new Date();
+const zeroPad = (num, places) => String(num).padStart(places, '0')
+window.onload = init();
+
+function nodeClick(id){
+	if (node == null){
+		node = id;
+		document.getElementById(node).setAttribute("fill", "blue");
+	}else if (node == id){
+		clear();
+	}else if (incMatrix[node][id] == 0){
+		incMatrix[node][id] = 1;
+		incMatrix[id][node] = 1;
+		createEdge(parseInt(id) < parseInt(node) ? id : node, parseInt(id) < parseInt(node) ? node : id);
+		clear();
+	}
+};
+
+function createEdge(s, t){
+	var edges = document.getElementById('edges');
+	var line = document.createElementNS(svgNS, 'line');
+	start = document.getElementById(s);
+	target = document.getElementById(t);
+	line.setAttribute("x1", start.getAttribute("cx"));
+	line.setAttribute("y1", start.getAttribute("cy"));
+	line.setAttribute("x2", target.getAttribute("cx"));
+	line.setAttribute("y2", target.getAttribute("cy"));
+	line.setAttribute("style", "stroke:rgb(0,0,0);stroke-width:2");
+	id = s+"-"+t;
+	line.setAttribute('id', id);
+	line.setAttribute('onclick', "deleteEdge('"+id+"')");
+	edges.appendChild(line);
+	var nodes = document.getElementById('nodes');
+};
+
+function deleteEdge(id){
+	var elem = id.split("-");
+	incMatrix[elem[0]][elem[1]] = 0;
+	incMatrix[elem[1]][elem[0]] = 0;
+	document.getElementById(id).remove();
+}
+
+function clear(){
+	if (node != null){
+		document.getElementById(node).setAttribute("fill", "black");
+		node = null;
+	}
+};
+
+function clearAll(){
+	for(var i = 0; i < N; i++){
+		for(var j = i+1; j < N; j++){
+			if (incMatrix[i][j]){
+				var id = i+"-"+j;
+				incMatrix[i][j] = 0;
+				incMatrix[j][i] = 0;
+				document.getElementById(id).remove();
+			}
+		}
+	}
+	clear();
+}
+
+function setKnownEdges(){
+	if (knownEdges == null){return;}
+	clearAll();
+	console.log(knownEdges + "");
+	for (var i = 0; i < N; i++){
+		for (var j = i+1; j < N; j++){
+			if (knownEdges[i][j] == 1){
+				createEdge(i, j);
+				incMatrix[i][j] = 1;
+				incMatrix[j][i] = 1;
+			}
+		}
+	}
+}
+
+function createNode(x, y, r){
+	var nodes = document.getElementById('nodes');
+	var circ = document.createElementNS(svgNS, 'circle');
+	circ.setAttribute('cx', x);
+	circ.setAttribute('cy', y);
+	circ.setAttribute('r', r);
+	id = ""+N;
+	circ.setAttribute('id', id)
+	circ.setAttribute('onclick', "nodeClick('"+id+"')");
+	nodes.appendChild(circ);	
+	N += 1;
+};
+
+function initNodes(){
+	rad = 3;
+	var coords = getCoords();
+	for (var i = 0; i < getNum(); i++){
+		createNode(coords[i][0], coords[i][1], rad);
+	}
+	incMatrix = new Array(N)
+	knownEdges = new Array(N);
+	for (var i = 0; i < N; i++){
+		incMatrix[i] = new Array(N).fill(0);
+		knownEdges[i] = new Array(N).fill(0);
+	}
+};
+
+function saveData(){
+	console.log(date.getFullYear() + zeroPad(date.getMonth()+1,2) + zeroPad(date.getDate()));
+	localStorage.setItem("version", version);
+	localStorage.setItem("date", date.getFullYear() + zeroPad(date.getMonth()+1,2) + zeroPad(date.getDate()));
+	localStorage.setItem("tries", num_tries);
+	var innerhtml = document.getElementById("try"+(num_tries)).innerHTML
+	localStorage.setItem("try"+(num_tries), innerhtml);
+
+}
+
+function loadData(){
+	var today = date.getFullYear() + zeroPad(date.getMonth()+1,2) + zeroPad(date.getDate());
+	if (today.localeCompare(localStorage.getItem("date")) == 0){
+		console.log("YEEES")
+		num_tries = localStorage.getItem("tries");
+		
+		for (var i = 0; i < num_tries; i++){
+			console.log("Restoring Try " +(i+1));
+			
+			var div = document.createElement("div");
+			div.setAttribute("class", "w3-center w3-block w3-border w3-quarter");
+			var par = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+			par.setAttribute('width',"100%")
+			par.setAttribute('viewBox', "0 0 100 100")
+			par.setAttribute('style', "max-width: 200px")
+			par.setAttribute('xmlns', "http://www.w3.org/2000/svg")
+			par.setAttribute('id', "try"+ (i+1));
+			par.innerHTML = localStorage.getItem("try"+(i+1));
+			div.appendChild(par);
+			document.getElementById('try').insertBefore(div, document.getElementById("tryrest"));
+		} 
+	}
+}
+
+function init(){
+	initNodes();
+	//localStorage.clear();
+	loadData();
+};
+
+function check(){
+	var degree_list = new Array(N).fill(0);
+	for (var i = 0; i < N; i++){
+		for (var j = i; j < N; j++){
+			degree_list[i] += incMatrix[i][j];
+			degree_list[j] += incMatrix[i][j];
+		}
+	}
+	degree_list.sort();
+	degree_list.reverse();
+	sequence = ""+degree_list;
+	//console.log(sequence + " vs " + getDegreeListToday());
+	if (sequence.localeCompare(getDegreeListToday())!=0){
+		alert("Degree sequence is not correct! Your input: " + sequence)
+		return;
+	}
+	colorMap = compareGraph(incMatrix);
+	var valid = true;
+	for (var i = 0; i < N; i++){
+		for (var j = i+1; j < N; j++){
+			if (incMatrix[i][j] == 0){continue;}
+			if (colorMap[i][j].localeCompare("green") != 0){
+				console.log(i + ", "+ j)
+				valid = false;
+			}else{
+				knownEdges[i][j] = 1;
+			}	
+		}
+	}
+	if (valid){
+		alert("Congratulations! You found the secret Graph in " + (1+num_tries) + (num_tries == 1 ? " try!" : " tries!"));
+	}
+	drawTry(colorMap);
+	saveData();
+}
+
+function drawTry(colorMap){
+	var rad = 2.5;
+	var div = document.createElement("div");
+	div.setAttribute("class", "w3-center w3-block w3-border w3-quarter");
+	var par = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+	//var parNS = par.namespaceURI;
+	var coords = getCoords();
+	par.setAttribute('width',"100%")
+	par.setAttribute('viewBox', "0 0 100 100")
+	par.setAttribute('style', "max-width: 200px")
+	par.setAttribute('xmlns', "http://www.w3.org/2000/svg")
+	par.setAttribute('id', "try"+ (++num_tries));
+	
+	//Draw Edges
+	for (var i = 0; i < N; i++){
+		for (var j = i+1; j < N; j++){
+			if(incMatrix[i][j]){
+				var line = document.createElementNS(svgNS, 'line');
+				line.setAttribute("x1", coords[i][0]);
+				line.setAttribute("y1", coords[i][1]);
+				line.setAttribute("x2", coords[j][0]);
+				line.setAttribute("y2", coords[j][1]);
+				line.setAttribute("style", "stroke:"+colorMap[i][j]+";stroke-width:1.5");	
+				par.append(line);
+			}
+		}
+	}
+	
+	//Create Nodes
+	for (var i = 0; i < N; i++){
+		var circ = document.createElementNS(svgNS, 'circle');
+		circ.setAttribute('cx', coords[i][0]);
+		circ.setAttribute('cy', coords[i][1]);
+		circ.setAttribute('r', rad);
+		circ.setAttribute('fill', colorMap[i][i]);
+		par.append(circ);
+	}
+	div.appendChild(par);
+	document.getElementById('try').insertBefore(div, document.getElementById("tryrest"));
+}
---
+++ src/graphToday.js
@@ -0,0 +1,81 @@
+				
+function getCoords(){
+	return coords;
+}
+
+
+function getAdjacencyList(){
+	return graph;
+}
+
+function getNum(){
+	return num;
+}
+
+function getDegreeListToday(){
+	var list = new Array(num).fill(0);
+	for (var i = 0; i < num; i++){
+		for (var j = i; j < num; j++){
+			list[i] += graph[i][j];
+			list[j] += graph[i][j];
+		}
+	}
+	list.sort().reverse();
+	return list;
+}
+
+
+function compareArrays(a, b){
+	if (a.length != b.length){return false;}
+	for (var i = 0; i < a.length; i++){
+		if (a[i] != b[i]){return false;}
+	}
+	return true;
+}
+
+function compareDegree(a, b){
+	if (a.length != b.length){return false;}
+	ca = 0;
+	cb = 0;
+	for (var i = 0; i < a.length; i++){
+		ca += a[i];
+		cb += b[i];
+	}
+	return ca == cb;
+}
+
+function compareGraph(input){
+	var result = new Array(num);
+
+	for (var i = 0; i < num; i++){
+		result[i] = new Array(num).fill("black");
+	}
+	//Check if there are correct nodes or if their degree is correct
+	for (var i = 0; i < num; i++){
+		if (compareArrays(input[i], graph[i])){
+			result[i][i] = "green";
+		}else if (compareDegree(input[i], graph[i])){
+			result[i][i] = "orange";
+		}
+	}
+	
+	//check if there are correct edges, or if their adjacent nodes have correct degree.
+	for (var i = 0; i < num; i++){
+		for (var j = i+1; j < num; j++){
+			if (!input[i][j]){continue;}
+			if (input[i][j] == graph[i][j]){
+				result[i][j] = "green";
+				result[j][i] = "green";
+			}else{
+				if((compareDegree(input[i],graph[i]) && compareDegree(input[j],graph[j])) ||
+					(compareDegree(input[i],graph[j]) && compareDegree(input[j],graph[i]))){	
+					result[i][j] = "orange";
+					result[j][i] = "orange";
+				}
+			}
+		}
+	}
+
+	console.log(result);
+	return result;
+}
