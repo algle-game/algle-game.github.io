8e40972b4facee9e2b55d9d136c6253219f579c6
Arne (Home) <aschmidt@ibr.cs.tu-bs.de>
2022-05-01 09:32:51 +0200

    some changes in layout

---
+++ js/overlay.js
@@ -0,0 +1,7 @@
+
+    function openNav() {
+      document.getElementById("overlaydiv").style.width = "100%";
+    }
+    function closeNav() {
+      document.getElementById("overlaydiv").style.width = "0%";
+    }
